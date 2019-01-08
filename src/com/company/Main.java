d2923e77c26d22ebd8db5e47401a95e674faa451
rshek8965 <rshek8965@gmail.com>
2019-01-08 11:38:22 -0500

    main code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,13 @@ public class Main {
         String[] ranks = {"1","2","3","4"};
         int[] pointValues = {1,2,3,4};
 
+        Deck x = new Deck(suits, ranks, pointValues);
+        System.out.println("Is empty? " + x.isEmpty());
+        System.out.println("Cards undealt? " + x.size());
+        x.shuffle();
+        while(x.size()>0)
+        {
+            System.out.println(x.deal());
+        }
     }
 }
